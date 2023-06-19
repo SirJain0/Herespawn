a25a1dbb19841393a674a07960fce5b29783f64a
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-06-19 13:38:25 -0400

    Get button functionality working (#1)

--- src/main/java/net/herespawn/Herespawn.java
+++ src/main/java/net/herespawn/Herespawn.java
@@ -2,7 +2,6 @@ package net.herespawn;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +9,17 @@ import org.slf4j.LoggerFactory;
 public class Herespawn implements ModInitializer {
     public static final String MOD_ID = "herespawn";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static Vec3d DEATH_POS = null;
+    public static boolean REQUESTS_RESPAWN_AT_DEATH = false;
 
     @Override
     public void onInitialize() {
         LOGGER.info("Initialized Herespawn");
 
         ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
-            newPlayer.teleport(100, 100, 100); // dummy coordinates
+            if (REQUESTS_RESPAWN_AT_DEATH && DEATH_POS != null) {
+                newPlayer.teleport(DEATH_POS.x, DEATH_POS.y, DEATH_POS.z);
+            }
         });
     }
 }
--- src/main/java/net/herespawn/mixin/DeathScreenMixin.java
+++ src/main/java/net/herespawn/mixin/DeathScreenMixin.java
@@ -1,44 +1,57 @@
 package net.herespawn.mixin;
 
 import ca.weblite.objc.Client;
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.herespawn.Herespawn;
 import net.minecraft.client.gui.screen.DeathScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.NbtByte;
-import net.minecraft.nbt.NbtInt;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Iterator;
+import java.util.List;
+
 @Mixin(DeathScreen.class)
 public class DeathScreenMixin extends Screen {
-	protected DeathScreenMixin(Text title) {
+	@Shadow private final List<ButtonWidget> buttons;
+
+	protected DeathScreenMixin(Text title, List<ButtonWidget> buttons) {
 		super(title);
+		this.buttons = buttons;
 	}
 
-	@Inject(at = @At("HEAD"), method = "init()V")
+	@Inject(at = @At("TAIL"), method = "init()V")
 	protected void init(CallbackInfo info) {
 		if (this.client == null) return;
+		final ClientPlayerEntity player = this.client.player;
+		if (player == null) return;
+		World world = player.getWorld();
 
-		// Adds button to death screen
-		this.addDrawableChild(ButtonWidget.builder(
+		Herespawn.REQUESTS_RESPAWN_AT_DEATH = false;
+		if (this.client == null) return;
+
+		ButtonWidget herespawnButton = ButtonWidget.builder(
 			Text.translatable("gui.herespawn.respawn_at_death_location"),
 			(button) -> {
-				ClientPlayerEntity player = this.client.player;
-				if (player == null) return;
-				Vec3d deathPos = player.getPos(); // to do: access this field
+				Herespawn.DEATH_POS = player.getPos();
+				Herespawn.REQUESTS_RESPAWN_AT_DEATH = true;
 
 				player.requestRespawn();
+				button.active = false;
 			}
 		)
 		.dimensions(this.width / 2 - 100, this.height / 4 + 120, 200, 20)
-		.build());
+		.build();
+
+		// Adds button to death screen
+		if (world.isClient) this.addDrawableChild(herespawnButton);
+		buttons.add(herespawnButton);
+		herespawnButton.active = false;
 	}
 }
