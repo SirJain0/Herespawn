dba0ca5b0fc6d3920df0ff031df2007e8bcd9cc4
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-06-24 12:27:34 -0500

    works on server

--- src/main/java/net/herespawn/mixin/DeathScreenMixin.java
+++ src/main/java/net/herespawn/mixin/DeathScreenMixin.java
@@ -1,5 +1,7 @@
 package net.herespawn.mixin;
 
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.herespawn.Herespawn;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.DeathScreen;
@@ -7,6 +9,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -31,27 +34,21 @@ public class DeathScreenMixin extends Screen {
 		if (this.client == null) return;
 		final ClientPlayerEntity player = this.client.player;
 		if (player == null) return;
-		World world = player.getWorld();
-
-		Herespawn.REQUESTS_RESPAWN_AT_DEATH = false;
+		Herespawn.REQUESTS_RESPAWN_AT_DEATH.remove(player.getId());
 
 		ButtonWidget herespawnButton = ButtonWidget.builder(
 			Text.translatable("gui.herespawn.respawn_at_death_location"),
 			(button) -> {
-				Herespawn.DEATH_POS = player.getPos();
-				Herespawn.REQUESTS_RESPAWN_AT_DEATH = true;
-
+//				Herespawn.DEATH_POS.put(player.getId(),player.getPos());
+//				Herespawn.REQUESTS_RESPAWN_AT_DEATH.put(player.getId(), true);
+				ClientPlayNetworking.send(Herespawn.REQUEST_RESPAWN_AT_DEATH, new PacketByteBuf(Unpooled.buffer()));
 				player.requestRespawn();
 				button.active = false;
 			}
 		)
 		.dimensions(this.width / 2 - 100, this.height / 4 + 120, 200, 20)
 		.build();
-
-		ClientConnection connection = MinecraftClient.getInstance().getNetworkHandler().getConnection();
-		if (connection != null && connection.isLocal()) this.addDrawableChild(herespawnButton);
-
+		this.addDrawableChild(herespawnButton);
 		buttons.add(herespawnButton);
-		herespawnButton.active = false;
 	}
 }
